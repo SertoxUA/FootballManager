f7598a8388576e747f1355baaad0edb56d7c0387
Anton Podolyan <sertox@i.ua>
2017-10-11 00:30:54 +0300

    Added and changed the first activity

---
+++ app/src/main/java/ua/sertox/football/Start.java
@@ -0,0 +1,41 @@
+package ua.sertox.football;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Start extends AppCompatActivity implements View.OnClickListener{
+
+    TextView appName;
+    TextView vName;
+    EditText editText;
+    Button nextOnStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        appName = (TextView) findViewById(R.id.appName);
+        vName = (TextView) findViewById(R.id.vName);
+        editText = (EditText) findViewById(R.id.editName);
+        nextOnStart = (Button) findViewById(R.id.nextOnStart);
+
+        appName.setText(R.string.app_name);
+        vName.setText(R.string.vName);
+        nextOnStart.setText(R.string.next);
+
+        nextOnStart.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent mainInt = new Intent(this, MainActivity.class);
+        startActivity(mainInt);
+    }
+}
